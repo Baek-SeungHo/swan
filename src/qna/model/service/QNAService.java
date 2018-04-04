ae50cd03525f00860c43c558969c85d1c2c92580
Back <Back@Back-PC>
2018-04-04 15:03:10 +0900

    qna 서블렛완성 서비스 작성해야됨

--- src/qna/model/service/QNAService.java
+++ src/qna/model/service/QNAService.java
@@ -1,5 +1,19 @@
 package qna.model.service;
 
+import java.util.ArrayList;
+
+import qna.model.vo.QNA;
+
 public class QNAService {
+
+	public int getListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<QNA> selectList(int currentPage, int limit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
