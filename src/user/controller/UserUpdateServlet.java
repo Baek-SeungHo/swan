e0e93b11795239fc0a988c675914fa23f5bd3f14
현나 <@user01>
2018-04-05 15:17:30 +0900

    aa

--- src/user/controller/UserUpdateServlet.java
+++ src/user/controller/UserUpdateServlet.java
@@ -15,7 +15,7 @@ import user.model.vo.User;
 /**
  * Servlet implementation class UserUpdateServlet
  */
-@WebServlet("/uupdate")
+@WebServlet("/uupdate.me")
 public class UserUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
